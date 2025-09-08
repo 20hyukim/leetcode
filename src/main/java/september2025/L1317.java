1365a17948d75a14a04a30811eb72b416ce809c2
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2025-09-08 22:30:05 +0900

    feat: 1317. Convert Integer to the Sum of Two No-Zero Integers

---
+++ src/main/java/september2025/L1317.java
@@ -0,0 +1,27 @@
+package september2025;
+
+class Solution1317 {
+    public int[] getNoZeroIntegers(int n) {
+        int[] answer = new int[2];
+
+        for (int i = 1; i < n; i ++) {
+            if (zero_exists(i) || zero_exists(n-i)) {
+                continue;
+            };
+            answer[0] = i;
+            answer[1] = n-i;
+            return answer;
+        }
+        return answer;
+    }
+
+    private boolean zero_exists(int x) {
+        while (x >= 10) {
+            if ( x % 10 == 0 ) {
+                return true;
+            };
+            x = x / 10;
+        }
+        return false;
+    }
+}
