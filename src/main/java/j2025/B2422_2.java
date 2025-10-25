51142b8b1387f38843489aa512ccea68278a05a5
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2025-10-26 00:00:48 +0900

    feat: 백준 2422번: 한윤정이 이탈리아에 가서 아이스크림을 사먹는데 (2)

--- src/main/java/j2025/B2422_2.java
+++ src/main/java/j2025/B2422_2.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+// 백준 2422번: 한윤정이 이탈리아에 가서 아이스크림을 사먹는데
+// 오답: for 문 범위 (0 ~ n) x // (1 ~ n+1)
 public class B2422_2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,9 +25,9 @@ public class B2422_2 {
         }
 
         int cnt = 0;
-        for (int i = 0; i < n ; i ++) {
-            for (int j = i+1; j < n; j++) {
-                for (int k = j+1; k < n; k++) {
+        for (int i = 1; i < n+1 ; i ++) {
+            for (int j = i+1; j < n+1; j++) {
+                for (int k = j+1; k < n+1; k++) {
                     if (bad[i][j] || bad[i][k] || bad[j][k]) {
                         continue;
                     }
